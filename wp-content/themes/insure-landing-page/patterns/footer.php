d56259ee2f1fc579d3d64abf65c2e9adae3766ca
Fer <fer.falcon.dg@gmail.com>
2026-05-31 00:48:30 -0300

    fix: add .editorconfig for consistent coding style and format adjustments in header and footer

--- wp-content/themes/insure-landing-page/patterns/footer.php
+++ wp-content/themes/insure-landing-page/patterns/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Title: Footer
  * Slug: insure-landing-page/footer
@@ -9,125 +10,139 @@
  * @package Insure_Landing_Page
  */
 
-$assets = trailingslashit( get_theme_file_uri( 'assets/images' ) );
+$assets = trailingslashit(get_theme_file_uri('assets/images'));
 ?>
 
 <!-- wp:group {"tagName":"footer","className":"insure-footer"} -->
 <div class="wp-block-group insure-footer"><!-- wp:group {"className":"insure-footer__inner insure-container"} -->
-<div class="wp-block-group insure-footer__inner insure-container"><!-- wp:group {"className":"insure-footer__brand"} -->
-<div class="wp-block-group insure-footer__brand"><!-- wp:image {"url":"<?php echo esc_url( $assets . 'logo.svg' ); ?>","alt":"Insure","href":"#","className":"insure-logo"} -->
-<figure class="wp-block-image insure-logo"><a href="#"><img src="<?php echo esc_url( $assets . 'logo.svg' ); ?>" alt="Insure"/></a></figure>
-<!-- /wp:image -->
+	<div class="wp-block-group insure-footer__inner insure-container"><!-- wp:group {"className":"insure-footer__brand"} -->
+		<div class="wp-block-group insure-footer__brand"><!-- wp:image {"url":"<?php echo esc_url($assets . 'logo.svg'); ?>","alt":"Insure","href":"#","className":"insure-logo"} -->
+			<figure class="wp-block-image insure-logo"><a href="#"><img src="<?php echo esc_url($assets . 'logo.svg'); ?>" alt="Insure" /></a></figure>
+			<!-- /wp:image -->
 
-<!-- wp:group {"tagName":"nav","ariaLabel":"Social links"} -->
-<nav aria-label="Social links" class="wp-block-group"><!-- wp:group {"className":"social-list"} -->
-<div class="wp-block-group social-list"><!-- wp:image {"url":"<?php echo esc_url( $assets . 'icon-facebook.svg' ); ?>","alt":"Facebook","href":"#","className":"social-list__icon"} -->
-<figure class="wp-block-image social-list__icon"><a href="#"><img src="<?php echo esc_url( $assets . 'icon-facebook.svg' ); ?>" alt="Facebook"/></a></figure>
-<!-- /wp:image -->
+			<!-- wp:group {"tagName":"nav","ariaLabel":"Social links"} -->
+			<nav aria-label="Social links" class="wp-block-group"><!-- wp:group {"className":"social-list"} -->
+				<div class="wp-block-group social-list"><!-- wp:image {"url":"<?php echo esc_url($assets . 'icon-facebook.svg'); ?>","alt":"Facebook","href":"#","className":"social-list__icon"} -->
+					<figure class="wp-block-image social-list__icon"><a href="#"><img src="<?php echo esc_url($assets . 'icon-facebook.svg'); ?>" alt="Facebook" /></a></figure>
+					<!-- /wp:image -->
 
-<!-- wp:image {"url":"<?php echo esc_url( $assets . 'icon-twitter.svg' ); ?>","alt":"Twitter","href":"#","className":"social-list__icon"} -->
-<figure class="wp-block-image social-list__icon"><a href="#"><img src="<?php echo esc_url( $assets . 'icon-twitter.svg' ); ?>" alt="Twitter"/></a></figure>
-<!-- /wp:image -->
+					<!-- wp:image {"url":"<?php echo esc_url($assets . 'icon-twitter.svg'); ?>","alt":"Twitter","href":"#","className":"social-list__icon"} -->
+					<figure class="wp-block-image social-list__icon"><a href="#"><img src="<?php echo esc_url($assets . 'icon-twitter.svg'); ?>" alt="Twitter" /></a></figure>
+					<!-- /wp:image -->
 
-<!-- wp:image {"url":"<?php echo esc_url( $assets . 'icon-pinterest.svg' ); ?>","alt":"Pinterest","href":"#","className":"social-list__icon"} -->
-<figure class="wp-block-image social-list__icon"><a href="#"><img src="<?php echo esc_url( $assets . 'icon-pinterest.svg' ); ?>" alt="Pinterest"/></a></figure>
-<!-- /wp:image -->
+					<!-- wp:image {"url":"<?php echo esc_url($assets . 'icon-pinterest.svg'); ?>","alt":"Pinterest","href":"#","className":"social-list__icon"} -->
+					<figure class="wp-block-image social-list__icon"><a href="#"><img src="<?php echo esc_url($assets . 'icon-pinterest.svg'); ?>" alt="Pinterest" /></a></figure>
+					<!-- /wp:image -->
 
-<!-- wp:image {"url":"<?php echo esc_url( $assets . 'icon-instagram.svg' ); ?>","alt":"Instagram","href":"#","className":"social-list__icon"} -->
-<figure class="wp-block-image social-list__icon"><a href="#"><img src="<?php echo esc_url( $assets . 'icon-instagram.svg' ); ?>" alt="Instagram"/></a></figure>
-<!-- /wp:image --></div>
-<!-- /wp:group --></nav>
-<!-- /wp:group --></div>
-<!-- /wp:group -->
+					<!-- wp:image {"url":"<?php echo esc_url($assets . 'icon-instagram.svg'); ?>","alt":"Instagram","href":"#","className":"social-list__icon"} -->
+					<figure class="wp-block-image social-list__icon"><a href="#"><img src="<?php echo esc_url($assets . 'icon-instagram.svg'); ?>" alt="Instagram" /></a></figure>
+					<!-- /wp:image -->
+				</div>
+				<!-- /wp:group -->
+			</nav>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
 
-<!-- wp:group {"className":"footer-nav-grid"} -->
-<div class="wp-block-group footer-nav-grid"><!-- wp:group {"tagName":"nav","ariaLabel":"Our company","className":"footer-nav"} -->
-<nav aria-label="Our company" class="wp-block-group footer-nav"><!-- wp:heading {"level":2} -->
-<h2 class="wp-block-heading">Our company</h2>
-<!-- /wp:heading -->
+		<!-- wp:group {"className":"footer-nav-grid"} -->
+		<div class="wp-block-group footer-nav-grid"><!-- wp:group {"tagName":"nav","ariaLabel":"Our company","className":"footer-nav"} -->
+			<nav aria-label="Our company" class="wp-block-group footer-nav"><!-- wp:heading {"level":2} -->
+				<h2 class="wp-block-heading">Our company</h2>
+				<!-- /wp:heading -->
 
-<!-- wp:list -->
-<ul class="wp-block-list"><!-- wp:list-item -->
-<li><a href="#how-we-work">How we work</a></li>
-<!-- /wp:list-item -->
+				<!-- wp:list -->
+				<ul class="wp-block-list"><!-- wp:list-item -->
+					<li><a href="#how-we-work">How we work</a></li>
+					<!-- /wp:list-item -->
 
-<!-- wp:list-item -->
-<li><a href="#">Why Insure?</a></li>
-<!-- /wp:list-item -->
+					<!-- wp:list-item -->
+					<li><a href="#">Why Insure?</a></li>
+					<!-- /wp:list-item -->
 
-<!-- wp:list-item -->
-<li><a href="#plans">View plans</a></li>
-<!-- /wp:list-item -->
+					<!-- wp:list-item -->
+					<li><a href="#plans">View plans</a></li>
+					<!-- /wp:list-item -->
 
-<!-- wp:list-item -->
-<li><a href="#">Reviews</a></li>
-<!-- /wp:list-item --></ul>
-<!-- /wp:list --></nav>
-<!-- /wp:group -->
+					<!-- wp:list-item -->
+					<li><a href="#">Reviews</a></li>
+					<!-- /wp:list-item -->
+				</ul>
+				<!-- /wp:list -->
+			</nav>
+			<!-- /wp:group -->
 
-<!-- wp:group {"tagName":"nav","ariaLabel":"Help me","className":"footer-nav"} -->
-<nav aria-label="Help me" class="wp-block-group footer-nav"><!-- wp:heading {"level":2} -->
-<h2 class="wp-block-heading">Help me</h2>
-<!-- /wp:heading -->
+			<!-- wp:group {"tagName":"nav","ariaLabel":"Help me","className":"footer-nav"} -->
+			<nav aria-label="Help me" class="wp-block-group footer-nav"><!-- wp:heading {"level":2} -->
+				<h2 class="wp-block-heading">Help me</h2>
+				<!-- /wp:heading -->
 
-<!-- wp:list -->
-<ul class="wp-block-list"><!-- wp:list-item -->
-<li><a href="#">FAQ</a></li>
-<!-- /wp:list-item -->
+				<!-- wp:list -->
+				<ul class="wp-block-list"><!-- wp:list-item -->
+					<li><a href="#">FAQ</a></li>
+					<!-- /wp:list-item -->
 
-<!-- wp:list-item -->
-<li><a href="#">Terms of use</a></li>
-<!-- /wp:list-item -->
+					<!-- wp:list-item -->
+					<li><a href="#">Terms of use</a></li>
+					<!-- /wp:list-item -->
 
-<!-- wp:list-item -->
-<li><a href="#">Privacy policy</a></li>
-<!-- /wp:list-item -->
+					<!-- wp:list-item -->
+					<li><a href="#">Privacy policy</a></li>
+					<!-- /wp:list-item -->
 
-<!-- wp:list-item -->
-<li><a href="#">Cookies</a></li>
-<!-- /wp:list-item --></ul>
-<!-- /wp:list --></nav>
-<!-- /wp:group -->
+					<!-- wp:list-item -->
+					<li><a href="#">Cookies</a></li>
+					<!-- /wp:list-item -->
+				</ul>
+				<!-- /wp:list -->
+			</nav>
+			<!-- /wp:group -->
 
-<!-- wp:group {"tagName":"nav","ariaLabel":"Contact","className":"footer-nav"} -->
-<nav aria-label="Contact" class="wp-block-group footer-nav"><!-- wp:heading {"level":2} -->
-<h2 class="wp-block-heading">Contact</h2>
-<!-- /wp:heading -->
+			<!-- wp:group {"tagName":"nav","ariaLabel":"Contact","className":"footer-nav"} -->
+			<nav aria-label="Contact" class="wp-block-group footer-nav"><!-- wp:heading {"level":2} -->
+				<h2 class="wp-block-heading">Contact</h2>
+				<!-- /wp:heading -->
 
-<!-- wp:list -->
-<ul class="wp-block-list"><!-- wp:list-item -->
-<li><a href="#">Sales</a></li>
-<!-- /wp:list-item -->
+				<!-- wp:list -->
+				<ul class="wp-block-list"><!-- wp:list-item -->
+					<li><a href="#">Sales</a></li>
+					<!-- /wp:list-item -->
 
-<!-- wp:list-item -->
-<li><a href="#">Support</a></li>
-<!-- /wp:list-item -->
+					<!-- wp:list-item -->
+					<li><a href="#">Support</a></li>
+					<!-- /wp:list-item -->
 
-<!-- wp:list-item -->
-<li><a href="#">Live chat</a></li>
-<!-- /wp:list-item --></ul>
-<!-- /wp:list --></nav>
-<!-- /wp:group -->
+					<!-- wp:list-item -->
+					<li><a href="#">Live chat</a></li>
+					<!-- /wp:list-item -->
+				</ul>
+				<!-- /wp:list -->
+			</nav>
+			<!-- /wp:group -->
 
-<!-- wp:group {"tagName":"nav","ariaLabel":"Others","className":"footer-nav"} -->
-<nav aria-label="Others" class="wp-block-group footer-nav"><!-- wp:heading {"level":2} -->
-<h2 class="wp-block-heading">Others</h2>
-<!-- /wp:heading -->
+			<!-- wp:group {"tagName":"nav","ariaLabel":"Others","className":"footer-nav"} -->
+			<nav aria-label="Others" class="wp-block-group footer-nav"><!-- wp:heading {"level":2} -->
+				<h2 class="wp-block-heading">Others</h2>
+				<!-- /wp:heading -->
 
-<!-- wp:list -->
-<ul class="wp-block-list"><!-- wp:list-item -->
-<li><a href="#">Careers</a></li>
-<!-- /wp:list-item -->
+				<!-- wp:list -->
+				<ul class="wp-block-list"><!-- wp:list-item -->
+					<li><a href="#">Careers</a></li>
+					<!-- /wp:list-item -->
 
-<!-- wp:list-item -->
-<li><a href="#">Press</a></li>
-<!-- /wp:list-item -->
+					<!-- wp:list-item -->
+					<li><a href="#">Press</a></li>
+					<!-- /wp:list-item -->
 
-<!-- wp:list-item -->
-<li><a href="#">Licenses</a></li>
-<!-- /wp:list-item --></ul>
-<!-- /wp:list --></nav>
-<!-- /wp:group --></div>
-<!-- /wp:group --></div>
-<!-- /wp:group --></div>
+					<!-- wp:list-item -->
+					<li><a href="#">Licenses</a></li>
+					<!-- /wp:list-item -->
+				</ul>
+				<!-- /wp:list -->
+			</nav>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
+</div>
 <!-- /wp:group -->
--- wp-content/themes/insure-landing-page/patterns/header.php
+++ wp-content/themes/insure-landing-page/patterns/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Title: Header
  * Slug: insure-landing-page/header
@@ -9,27 +10,30 @@
  * @package Insure_Landing_Page
  */
 
-$assets = trailingslashit( get_theme_file_uri( 'assets/images' ) );
+$assets = trailingslashit(get_theme_file_uri('assets/images'));
 ?>
 
 <!-- wp:group {"tagName":"header","className":"insure-header"} -->
 <div class="wp-block-group insure-header"><!-- wp:group {"className":"insure-header__inner insure-container"} -->
-<div class="wp-block-group insure-header__inner insure-container"><!-- wp:image {"url":"<?php echo esc_url( $assets . 'logo.svg' ); ?>","alt":"Insure","href":"#","className":"insure-logo"} -->
-<figure class="wp-block-image insure-logo"><a href="#"><img src="<?php echo esc_url( $assets . 'logo.svg' ); ?>" alt="Insure"/></a></figure>
-<!-- /wp:image -->
+	<div class="wp-block-group insure-header__inner insure-container"><!-- wp:image {"url":"<?php echo esc_url($assets . 'logo.svg'); ?>","alt":"Insure","href":"#","className":"insure-logo"} -->
+		<figure class="wp-block-image insure-logo"><a href="#"><img src="<?php echo esc_url($assets . 'logo.svg'); ?>" alt="Insure" /></a></figure>
+		<!-- /wp:image -->
 
-<!-- wp:navigation {"overlayMenu":"always","icon":"menu","hasIcon":true,"ariaLabel":"Primary navigation","className":"primary-nav","layout":{"type":"flex","justifyContent":"right"}} -->
-<!-- wp:navigation-link {"label":"How we work","url":"#how-we-work"} /-->
+		<!-- wp:navigation {"overlayMenu":"always","icon":"menu","hasIcon":true,"ariaLabel":"Primary navigation","className":"primary-nav","layout":{"type":"flex","justifyContent":"right"}} -->
+		<!-- wp:navigation-link {"label":"How we work","url":"#how-we-work"} /-->
 
-<!-- wp:navigation-link {"label":"Blog","url":"#"} /-->
+		<!-- wp:navigation-link {"label":"Blog","url":"#"} /-->
 
-<!-- wp:navigation-link {"label":"Account","url":"#"} /-->
+		<!-- wp:navigation-link {"label":"Account","url":"#"} /-->
 
-<!-- wp:buttons {"className":"primary-nav__actions"} -->
-<div class="wp-block-buttons primary-nav__actions"><!-- wp:button {"url":"#plans","className":"primary-nav__button"} -->
-<div class="wp-block-button primary-nav__button"><a class="wp-block-button__link wp-element-button" href="#plans">View plans</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons -->
-<!-- /wp:navigation --></div>
-<!-- /wp:group --></div>
+		<!-- wp:buttons {"className":"primary-nav__actions"} -->
+		<div class="wp-block-buttons primary-nav__actions"><!-- wp:button {"url":"#plans","className":"primary-nav__button"} -->
+			<div class="wp-block-button primary-nav__button"><a class="wp-block-button__link wp-element-button" href="#plans">View plans</a></div>
+			<!-- /wp:button -->
+		</div>
+		<!-- /wp:buttons -->
+		<!-- /wp:navigation -->
+	</div>
+	<!-- /wp:group -->
+</div>
 <!-- /wp:group -->
